31908e4d8541a66043a76caedf2c1ddb330702e8
Likes123 <2928606762@qq.com>
2020-04-14 22:41:43 +0800

    v6.0 highlight eudic words, filter simple words

--- wordGenerator.py
+++ wordGenerator.py
@@ -3,6 +3,8 @@ import openpyxl
 import os.path
 import re
 from googletrans import Translator
+from openpyxl.styles import Font
+from openpyxl.styles.colors import RED
 # from translate import Translator
 
 
@@ -36,6 +38,13 @@ def write_xml(file_name, ret):
     # 翻译后变成中文逗号，不用空格分隔，避免上下文联系，，，，实际上更复杂，试验后发现使用句号上下文联系最少，如果使用特殊符号，google翻译会自动清楚，怪google太智能 orz
     trans_result_list = trans_result.split("。")
 
+    tree = ET.parse("data/udicWord.xml")
+    udic = set()
+    for elem in tree.iter(tag='CustomizeListItem'):
+        udic.add(elem.get("word"))
+
+    color_font = Font(color=RED)
+
     i = 0
     index = 1
     for (key, value) in ret.items():
@@ -49,6 +58,10 @@ def write_xml(file_name, ret):
             ws.cell(index, 1, key)
             ws.cell(index, 2, value)
             ws.cell(index, 3, temp)
+            if key in udic:
+                ws.cell(index, 1).font = color_font
+                ws.cell(index, 2).font = color_font
+                ws.cell(index, 3).font = color_font
             index += 1
         i = i+1
 
@@ -56,15 +69,15 @@ def write_xml(file_name, ret):
     wb.save(filename=(file_name))
 
 
-def auto_generate_word(source_file, delete_file, is_use_eudic, filter_times):
+def auto_generate_word(source_file, delete_file,  filter_times):
     text = dict()
     file = open("data/"+source_file, "r")
     file_text = file.read()
     file_text = file_text.lower()
 
     file_text = re.sub('-[\n\t]+', '', file_text)
-    word_list = re.findall('[a-zA-Z\-\'][a-zA-Z\-\'][a-zA-Z\-\']+',
-                           file_text)  # 至少3个字符的才能入选，一个单词和两个单词太简单，而且容易导致翻译依赖
+    word_list = re.findall('[a-zA-Z\-\'][a-zA-Z\-\'][a-zA-Z\-\'][a-zA-Z\-\']+',
+                           file_text)  # 至少4个字符的才能入选，一个单词和两个单词太简单，而且容易导致翻译依赖，比如：etc.
     for word in word_list:
         if word not in text:
             text[word] = 1
@@ -98,49 +111,49 @@ def auto_generate_word(source_file, delete_file, is_use_eudic, filter_times):
     for elem in tree_city.iter(tag='City'):
         delete_name.add(elem.get("Name").lower())
 
+    file5 = open("data/simple word lists.txt", "r")
+    delete_simple_words_text = file5.read()
+    delete_simple_words_list = delete_simple_words_text .split("\n")
+    delete_simple_words = set()
+    for temp in delete_simple_words_list:
+        delete_simple_words.add(temp)
+    # print(delete_simple_words)
+
     # print(delete_name)
 
     ret = dict()
-    if is_use_eudic:
-        tree = ET.parse("data/udicWord.xml")
-        dic = set()
-        for elem in tree.iter(tag='CustomizeListItem'):
-            dic.add(elem.get("word"))
-
-        dic = dic-delete_text-delete_name
-        for (key, value) in text.items():
-            if key in dic and value > filter_times:  # 频率大于2
-                ret[key] = value
-    else:
-        for (key, value) in text.items():
-            if key not in delete_text and key not in delete_name and value > filter_times:
-                ret[key] = value
+    for (key, value) in text.items():
+        if key not in delete_text and key not in delete_name and key not in delete_simple_words and value > filter_times:
+            ret[key] = value
 
     file.close()
     file2.close()
     file3.close()
     file4.close()
+    file5.close()
     return ret
 
 
 def main():
     # need set
     ##############################
-    is_use_eudic = False
+    # is_use_eudic = False
     # source_file = "17.txt"
-    source_file = "thinking in java.txt"
+    source_file = "12 rules for life.txt"
     delete_file = "delete.txt"
-    filter_times = 3  # 出现频率小于等于3不出现
+    filter_times = 0  # 出现频率小于等于3不出现
     ##############################
     print("============================================")
     print("源文件："+source_file)
     print("不显示的常用单词表："+delete_file)
+    print("删除简单单词：data/simple word lists.txt")
+    print("高亮欧路词典标记单词")
     print("只显示出现"+str(filter_times)+"以上的单词")
-    print("是否只匹配给定词典："+str(is_use_eudic))
+    # print("是否只匹配给定词典："+str(is_use_eudic))
     print("============================================")
     print("生成单词列表中...")
     ret = auto_generate_word(source_file, delete_file,
-                             is_use_eudic, filter_times)
+                             filter_times)
     print("翻译和写入Excel文件...")
     write_xml("word_lists", ret)
     print("完成！")
